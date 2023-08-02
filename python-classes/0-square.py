a956f15bfd9b59f4bfe7ad707b74e4b25ceff9f4
bsrBe <bsr7349@gmail.com>
2023-08-02 00:24:08 -0700

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,3 +10,42 @@ class Square:
         :type size: int
         """
         self.__size = size
+
+    @property
+    def dict_(self):
+        """
+        Returns the dictionary representation of a Square instance.
+
+        :return: The dictionary representation of the Square instance.
+        :rtype: dict
+        """
+        return {'size': self.__size}
+
+# Test cases
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_)
+
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_)
+
+mysquare = Square(89)
+print(type(mysquare))
+print(mysquare.dict_)
+
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_)
+
+try:
+    print(mysquare.size)
+except AttributeError as e:
+    print(e)
+
+mysquare = Square(3)
+print(type(mysquare))
+try:
+    print(mysquare._size)
+except AttributeError as e:
+    print(e)
