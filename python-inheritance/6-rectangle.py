34bc4d042cdf5050ebbae32c48911fa23c369549
bsrBe <bsr7349@gmail.com>
2023-08-03 06:11:01 -0700

    Update 6-rectangle.py

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,21 @@
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
